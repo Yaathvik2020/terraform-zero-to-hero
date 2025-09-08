e1d5929e5c8e4d9f044bbcad05a3d4aa1c7489a9
Yaathvik2020 <chinnayaathvik@gmail.com>
2025-09-08 17:49:34 +0000

    trg

---
+++ Day-3/modules/chinna_aws/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+    description = "enter Ami_value"
+    type = string
+  
+}
+variable "instance_type_value" {
+    description = "enetere instance_type value"
+    type = string
+}
